b2a4662192147b8ea05df769c0f3681e87ffd8f1
mayankgaur1301 <gaurm924@gmail.com>
2025-08-19 23:01:05 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose || {
+  conn: null,
+  promise: null,
+};
+
+export default async function connectDB() {
+  if (cached.conn) return cached.conn; 
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((mongoose) => mongoose);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    throw error; // re-throw so app knows it failed
+  }
+
+  return cached.conn; 
+}
